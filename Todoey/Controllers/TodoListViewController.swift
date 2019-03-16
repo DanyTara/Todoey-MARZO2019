0f9dcd36b97ff14cd48074684ed7216919609349
Daniela Tarantini <tarantinidaniela@gmail.com>
2019-03-16 12:44:32 +0100

    Reflector del codice, aggiunte funzione della SearchBar (textDidChage e Dismiss)

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,13 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     
-//    creo la directory per i miei dati (array) documens > Items.plist > aggiungo elementi (appending)
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     
    
@@ -21,9 +21,17 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print(dataFilePath)
+//        //    creo la directory per i miei dati (array) documens > Items.plist > aggiungo elementi (appending)
+//        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist"))
         
+//        8 - scrivo qui la costante request (che è il mio array intero senza filtro) e la inserisco come parametro del loadItems
+        //        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        //        loadItems(with: request)
+        
+//         10 - torno al loadItem senza paramenbtro perché ho aggiunyo quello di default
+
         loadItems()
+        
         
         }
         
@@ -52,11 +60,18 @@ class TodoListViewController: UITableViewController {
 //    MARK - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+  
+     //        cruD: DELETE ( per ora rimane commentato perché teniamo la spunta del DONE
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
+        //        crUd: UPDATE
+        
         
 //  operatore booleano quindi con il simbolo !(NOT) si inverte
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        saveData()
+        saveItems()
         
         
 //        mi serve per togliere in grigio dalla riga selezionata
@@ -75,20 +90,18 @@ class TodoListViewController: UITableViewController {
        
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
+            //        Crud: CREATE
+            
 //        what will happen once the user clicks the Add Items Button on our UIAlert
-            
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
-            
+            newItem.done = false
             self.itemArray.append(newItem)
             
-            self.saveData()
-            
-            
-        }
+            self.saveItems()
+            }
         
-        
-        alert.addTextField { (alertTextField) in
+            alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
            
@@ -101,42 +114,93 @@ class TodoListViewController: UITableViewController {
     
 //    Model Manipulation methods
     
-//    ENCODE e DECODER permette di convertire i miei dati (array) in un plist e riconvertirli (decode) in dati
-    func saveData() {
-        let encoder = PropertyListEncoder()
+
+    func saveItems() {
         
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+            print("Error saving context \(error)")
         }
         
         tableView.reloadData()
+        }
+    
+    
+    //        cRud: READ
+    
+//    4 -modifico la funzione scrivendo il parametro requesto DI TIPO NSFetchRequest che ritorna un array di tipo Item
+//    func loadItems() {
+//    5 - aggiungo with che è parametro esterno, request è paramentro interno. Ovvero quello chiamato all'interno della funzione
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+
+//      7 - commento questa linea perché aggiungendo il parametro la richiesta è chiara e passo questa costante nell'overlaod dove devo ricaricare la tableview completa di tutti gli elementi
+//        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        //        - 9 : aggiungo il parametro di DEFAULt = Item.fetchRequest()
+//        func loadItems(with request: NSFetchRequest<Item>) {
+
+        do {
+        itemArray = try context.fetch(request)
+        } catch  {
+            print("\(error)")
+        }
+        
+        tableView.reloadData()
+        
     }
     
-    func loadItems() {
+    
+}
+
+//    MARK: - Search bar methods
+
+
+extension TodoListViewController: UISearchBarDelegate {
+    
+        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
-//        creo il percorso
-        if let data = try? Data(contentsOf: dataFilePath!){
-//            creo una nuova variabile DECODER
-            let decoder = PropertyListDecoder()
+            let request : NSFetchRequest<Item> = Item.fetchRequest()
             
-            do {
-//              nel mio array metto il risultato del DECODE (specifico l'infer della mia classe array>Item dal percorso sopra -data-)
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error decoding item array \(error)")
+//            let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+//            request.predicate = predicate
+   //        1- QUESTO DIVENTA COSì:
+            request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+            
+//            let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+//            request.sortDescriptors = [sortDescriptor]
+            // 2 - QUESTO DIVENTA COSì:
+            request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+            
+        
+//        do {
+//            itemArray = try context.fetch(request)
+//        } catch  {
+//            print("\(error)")
+//        }
+//        3 - ANZICHè RISCRVERE IL context.fetchRequest modifico la funzione loadItems con il parametro request
+        loadItems(with: request)
+//        6 -    e posso cancellare il reloadData che è gia contenuto nella funzione loadItems
+//        tableView.reloadData()
+        
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
             }
-            
+           
         }
         
     }
     
+    
+    
 }
 
-    
-    
     
     
 
